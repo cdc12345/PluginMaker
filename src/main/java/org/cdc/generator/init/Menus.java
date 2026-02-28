458b5977677a404fd7ad520baae8f94f74658b7d
cdc12345 <cdccome@qq.com>
2026-02-28 20:52:28 +0800

    new function: load language properties file

--- src/main/java/org/cdc/generator/init/Menus.java
+++ src/main/java/org/cdc/generator/init/Menus.java
@@ -1,6 +1,7 @@
 package org.cdc.generator.init;
 
 import net.mcreator.ui.MCreator;
+import net.mcreator.ui.dialogs.file.FileDialogs;
 import net.mcreator.ui.init.L10N;
 import org.cdc.generator.elements.DataListModElement;
 import org.cdc.generator.ui.elements.DataListModElementGUI;
@@ -9,7 +10,11 @@ import javax.swing.*;
 import javax.swing.event.MenuEvent;
 import javax.swing.event.MenuListener;
 import java.awt.datatransfer.StringSelection;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.Properties;
 import java.util.stream.Stream;
 
 public class Menus {
@@ -19,6 +24,22 @@ public class Menus {
 
 	public static void registerAllMenus(MCreator mCreator) {
 		mCreator.getMainMenuBar().add(PLUGIN_MAKER);
+		JMenuItem loadLangFromExternal = new JMenuItem("Load properties lang file");
+		loadLangFromExternal.addActionListener(a -> {
+			var file = FileDialogs.getOpenDialog(mCreator, new String[] { "*.properties" });
+			Properties properties = new Properties();
+			try {
+				properties.load(new FileReader(file));
+				var langmap = mCreator.getWorkspace().getLanguageMap();
+				var en = langmap.get("en_us");
+				for (Map.Entry<Object, Object> objectObjectEntry : properties.entrySet()) {
+					en.put(objectObjectEntry.getKey().toString(), objectObjectEntry.getValue().toString());
+				}
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		});
+		PLUGIN_MAKER.add(loadLangFromExternal);
 
 		mCreator.getMainMenuBar().add(DATALIST_UTILS);
 		JMenu calculateTypes = L10N.menu("menus.datalist_utils.calculate_types");
@@ -52,12 +73,12 @@ public class Menus {
 			}
 		});
 		JMenu builtInEntries = L10N.menu("menus.datalist_utils.builtin_entries");
-		Stream.of("_default","_mcreator_map_template","_bypass_prefix").forEach(a->{
+		Stream.of("_default", "_mcreator_map_template", "_bypass_prefix").forEach(a -> {
 			JMenuItem menuItem = new JMenuItem(a);
-			menuItem.addActionListener(even->{
+			menuItem.addActionListener(even -> {
 				if (mCreator.getTabs().getCurrentTab()
 						.getContent() instanceof DataListModElementGUI dataListModElementGUI) {
-					dataListModElementGUI.entries.add(new DataListModElement.DataListEntry(a,null,null,null,null));
+					dataListModElementGUI.entries.add(new DataListModElement.DataListEntry(a, null, null, null, null));
 					dataListModElementGUI.refreshTable();
 				}
 			});
