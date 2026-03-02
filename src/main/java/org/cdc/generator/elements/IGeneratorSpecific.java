80b273729c0a2752cf8ae0440d1b6183a322978c
cdc12345 <cdccome@qq.com>
2026-03-02 17:54:59 +0800

    update

--- src/main/java/org/cdc/generator/elements/IGeneratorSpecific.java
+++ src/main/java/org/cdc/generator/elements/IGeneratorSpecific.java
@@ -1,5 +1,8 @@
 package org.cdc.generator.elements;
 
+import com.google.j2objc.annotations.UsedByReflection;
+
 public interface IGeneratorSpecific {
+    @UsedByReflection
     String getGeneratorName();
 }
