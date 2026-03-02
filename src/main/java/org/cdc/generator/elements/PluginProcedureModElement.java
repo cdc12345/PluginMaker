345a2967be47795c1ba7bb52f334efc16d20b2bb
cdc12345 <cdccome@qq.com>
2026-03-02 20:40:38 +0800

    todo plugin procedure

---
+++ src/main/java/org/cdc/generator/elements/PluginProcedureModElement.java
@@ -0,0 +1,11 @@
+package org.cdc.generator.elements;
+
+import net.mcreator.element.GeneratableElement;
+import net.mcreator.workspace.elements.ModElement;
+
+//Todo
+public class PluginProcedureModElement extends GeneratableElement {
+    public PluginProcedureModElement(ModElement element) {
+        super(element);
+    }
+}
